069be6d0e9ddacf9b3d1acb442dd0d5b185a76e6
John McLear <john@mclear.co.uk>
2013-12-19 00:43:44 +0000

    not properly formatting all chars but general gist is in place

---
+++ hooks.js
@@ -0,0 +1,23 @@
+var path = require('path'), 
+    express = require('ep_etherpad-lite/node_modules/express'),
+    eejs = require("ep_etherpad-lite/node/eejs");
+
+exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
+  args.content = args.content + eejs.require("ep_special_characters/templates/editbarButtons.ejs", {}, module);
+  return cb();
+}
+
+exports.eejsBlock_body = function (hook_name, args, cb) {
+  args.content = args.content + eejs.require("ep_special_characters/templates/modals.ejs", {}, module);
+  return cb();
+}
+
+exports.eejsBlock_scripts = function (hook_name, args, cb) {
+  args.content = args.content + eejs.require("ep_special_characters/templates/scripts.ejs", {}, module);
+  return cb();
+}
+
+exports.eejsBlock_styles = function (hook_name, args, cb) {
+  args.content = args.content + eejs.require("ep_special_characters/templates/styles.ejs", {}, module);
+  return cb();
+}
