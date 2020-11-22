96f79b9f41390575471bda96e57390775b34b7f6
John McLear <john@mclear.co.uk>
2020-11-22 17:47:49 +0000

    autofixes from Etherpad checkPlugins.js

--- hooks.js
+++ hooks.js
@@ -1,28 +1,27 @@
-var path = require('path'), 
-    express = require('ep_etherpad-lite/node_modules/express'),
-    eejs = require("ep_etherpad-lite/node/eejs");
+const path = require('path');
+const express = require('ep_etherpad-lite/node_modules/express');
+const eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_special_characters/templates/editbarButtons.ejs", {}, module);
+  args.content += eejs.require('ep_special_characters/templates/editbarButtons.ejs', {}, module);
   return cb();
-}
+};
 
 exports.eejsBlock_body = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_special_characters/templates/modals.ejs", {}, module);
+  args.content += eejs.require('ep_special_characters/templates/modals.ejs', {}, module);
   return cb();
-}
+};
 
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_special_characters/templates/scripts.ejs", {}, module);
+  args.content += eejs.require('ep_special_characters/templates/scripts.ejs', {}, module);
   return cb();
-}
+};
 
 exports.eejsBlock_styles = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_special_characters/templates/styles.ejs", {}, module);
+  args.content += eejs.require('ep_special_characters/templates/styles.ejs', {}, module);
   return cb();
-}
+};
 
-exports.eejsBlock_dd_insert = function (hook_name, args, cb){
-  args.content = args.content + eejs.require('ep_special_characters/templates/special_characters_menu.ejs');
-}
-
+exports.eejsBlock_dd_insert = function (hook_name, args, cb) {
+  args.content += eejs.require('ep_special_characters/templates/special_characters_menu.ejs');
+};
--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,33 @@
-$(document).ready(function () {
-  $(".insertSpecialCharacter").click(function () {
-    var module = $("#specialCharactersModal");
-    if (module.css('display') != "none") {
-      module.slideUp("fast");
+$(document).ready(() => {
+  $('.insertSpecialCharacter').click(() => {
+    const module = $('#specialCharactersModal');
+    if (module.css('display') != 'none') {
+      module.slideUp('fast');
     } else {
-      module.slideDown("fast");
+      module.slideDown('fast');
     }
   });
 
-  $("#cancelspecialCharacters").click(function () {
-    $("#specialCharactersModal").slideUp("fast");
+  $('#cancelspecialCharacters').click(() => {
+    $('#specialCharactersModal').slideUp('fast');
   });
-  $("#specialCharactersModal").on('click', '.specialChar', function(){
-    var char = ($(this).text());
+  $('#specialCharactersModal').on('click', '.specialChar', function () {
+    const char = ($(this).text());
     $('.usedSpecialCharacters').append(this);
     $('.usedSpecialCharactersLabel').show();
-    var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
-    $("#specialCharactersModal").slideUp("fast");
-    return padeditor.ace.callWithAce(function (ace) {
+    const padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
+    $('#specialCharactersModal').slideUp('fast');
+    return padeditor.ace.callWithAce((ace) => {
       rep = ace.ace_getRep();
       ace.ace_replaceRange(rep.selStart, rep.selEnd, char);
-        ace.ace_focus();
-    }, "specialCharacters");
-    
+      ace.ace_focus();
+    }, 'specialCharacters');
   });
 
-  var i = 0;
-  var total = 65535;
-  while (i <= 5000){
-    $('.specialChars').append("<li class='specialChar'>&#"+i+"</li>");
+  let i = 0;
+  const total = 65535;
+  while (i <= 5000) {
+    $('.specialChars').append(`<li class='specialChar'>&#${i}</li>`);
     i++;
   }
-
 });
